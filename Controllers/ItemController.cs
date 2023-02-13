fe996ce399bf5c9ac51aea6b9ba1f74cdf80f4e1
Andy Gorbould <gorbould@mail.com>
2023-02-13 12:51:59 +0100

    added Get for itemType

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -7,7 +7,7 @@ namespace RankingApp.Controllers
     [Route("[controller]")]
     public class ItemController : ControllerBase
     {
-        private static readonly IEnumerable<ItemModel> items = new[]
+        private static readonly IEnumerable<ItemModel> Items = new[]
         {
             new ItemModel{Id =1, Title = "The Godfather", ImageId=1, Ranking=0,ItemType=1 },
             new ItemModel{Id =2, Title = "Highlander", ImageId=2, Ranking=0,ItemType=1 },
@@ -17,7 +17,7 @@ namespace RankingApp.Controllers
             new ItemModel{Id =6, Title = "Rear Window", ImageId=6, Ranking=0,ItemType=1 },
             new ItemModel{Id =7, Title = "Road House", ImageId=7, Ranking=0,ItemType=1 },
             new ItemModel{Id =8, Title = "The Shawshank Redemption", ImageId=8, Ranking=0,ItemType=1 },
-            new ItemModel{Id =9, Title = "Star Treck IV", ImageId=9, Ranking=0,ItemType=1 },
+            new ItemModel{Id =9, Title = "Star Trek IV", ImageId=9, Ranking=0,ItemType=1 },
             new ItemModel{Id =10, Title = "Superman 4", ImageId=10, Ranking=0,ItemType=1 },
             new ItemModel{Id = 11, Title = "Abbey Road", ImageId=11, Ranking=0,ItemType=2 },
             new ItemModel{Id = 12, Title = "Adrenalize", ImageId=12, Ranking=0,ItemType=2 },
@@ -29,6 +29,13 @@ namespace RankingApp.Controllers
             new ItemModel{Id = 18, Title = "Rust in Peace", ImageId=18, Ranking=0,ItemType=2 },
             new ItemModel{Id = 19, Title = "St. Anger", ImageId=19, Ranking=0,ItemType=2 },
             new ItemModel{Id = 20, Title = "The Final Countdown", ImageId=20, Ranking=0,ItemType=2 }
+        };
+
+        [HttpGet("{itemType:int}")]
+        public ItemModel[] Get(int itemType)
+        {
+            ItemModel[] items = Items.Where(i => i.ItemType == itemType).ToArray();
+            return items;
         };
     }
 }
