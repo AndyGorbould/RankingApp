aa3e19d2d3e24e7ead503f7ac2863e34875e9104
Andy Gorbould <gorbould@mail.com>
2023-02-16 17:49:37 +0100

    fixed missing part of createCellsForRow func in RankingGrid

--- ClientApp/src/components/RankingGrid.js
+++ ClientApp/src/components/RankingGrid.js
@@ -34,7 +34,7 @@ const RankingGrid = ({ items, imgArr, drag, allowDrop, drop }) => {
         const numCells = 5;
 
         for (var a = 1; a <= numCells; a++) {
-            rankNum = (a === 1) ? 0 : (numCells - 1) + a - rowNum;
+            rankNum = (a === 1) ? 0 : (numCells * (rowNum - 1)) + a - rowNum;
 
             if (rowNum === 1) {
                 currCollection = cellCollectionTop;
